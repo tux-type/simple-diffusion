9187329c97afdf5abe860f1cb03591b4060cda57
Tux <177398239+tux-type@users.noreply.github.com>
2024-10-20 16:23:50 +0100

    Replace hf datasets with ray data

--- train_mnist.py
+++ train_mnist.py
@@ -1,9 +1,18 @@
+# ruff: noqa: E402
+import ray
+import ray.data
+import logging
+
+ray.data.DataContext.get_current().enable_progress_bars = False
+logging.getLogger("ray.data").setLevel(logging.WARNING)
+
+ray.init(num_cpus=12, num_gpus=1)
+
 import jax
 import jax.numpy as jnp
 import matplotlib.image
 import numpy as np
 import optax
-from datasets import Dataset, load_dataset
 from flax import nnx
 from tqdm import tqdm
 
@@ -11,8 +20,7 @@ from diffusion.ddpm import DDPM, ddpm_schedule
 from diffusion.unet import UNet
 
 
-def normalise_batch(samples):
-    samples = samples.copy()
+def normalise_batch(samples: dict[str, np.ndarray]) -> dict[str, np.ndarray]:
     data = np.array(samples["image"]).astype(np.float32)
 
     mean_per_sample = np.mean(data, axis=(1, 2), keepdims=True)
@@ -49,8 +57,6 @@ def train_step(
 
 
 def train_mnist(num_epochs: int):
-    # Set seed?
-
     init_rngs = nnx.Rngs(params=0)
     training_rngs = nnx.Rngs(times=1, noise=2)
     sampling_rngs = nnx.Rngs(noise=3)
@@ -60,10 +66,14 @@ def train_mnist(num_epochs: int):
         epsilon_model=UNet(1, 1, num_features=128, rngs=init_rngs),
         noise_schedule=noise_schedule,
         num_steps=1000,
+        device=jax.devices("gpu")[0],
     )
 
-    dataset: Dataset = load_dataset("mnist", keep_in_memory=True, split="train").with_format("jax")  # type: ignore
-    normalised_dataset = dataset.map(normalise_batch, batched=True)
+    dataset = (
+        ray.data.read_images("data/mnist", override_num_blocks=12)
+        .map_batches(normalise_batch)
+        .materialize()
+    )
 
     optimiser = nnx.Optimizer(ddpm, optax.adam(2e-4))
 
@@ -71,32 +81,35 @@ def train_mnist(num_epochs: int):
 
     metrics_history = {"train_loss": []}
     for i in range(num_epochs):
-        normalised_dataset.shuffle()
-        progress_bar = tqdm(normalised_dataset.iter(batch_size=128, drop_last_batch=True))
+        dataset = dataset.random_shuffle().materialize()
+        progress_bar = tqdm(
+            dataset.iter_batches(
+                prefetch_batches=1, batch_size=128, batch_format="numpy", drop_last=True
+            )
+        )
         for batch in progress_bar:
-            # for step, batch in enumerate(normalised_dataset.iter(batch_size=128, drop_last_batch=True)):
-            x = batch["image"][:, :, :, None]  # type: ignore
+            x = jnp.array(batch["image"])[:, :, :, None]
             train_step(ddpm, optimiser, metrics, training_rngs, x)
 
             for metric, value in metrics.compute().items():
                 metrics_history[f"train_{metric}"].append(value)
-            # print(f"[train] step: {step}, " f"loss: {metrics_history['train_loss'][-1]}, ")
             loss = metrics_history["train_loss"][-1]
             progress_bar.set_description(f"loss: {loss:.4f}")
 
-        x_0 = ddpm.sample(
-            num_images=16,
-            image_dimensions=(32, 32),
-            image_channels=1,
-            rngs=sampling_rngs,
-            device=jax.devices("gpu")[0],
-        )
-        # Squeeze since grayscale
-        matplotlib.image.imsave(
-            f"imgs/image_{i}.png",
-            jnp.concatenate(x_0, axis=1).squeeze(axis=2).astype(jnp.uint8),
-            cmap="binary",
-        )
+        if i % 5 == 0 or i == (num_epochs - 1):
+            print(f"Sampling Epoch: {i}")
+            x_0 = ddpm.sample(
+                num_images=16,
+                image_dimensions=(32, 32),
+                image_channels=1,
+                rngs=sampling_rngs,
+            )
+            # Squeeze since grayscale
+            matplotlib.image.imsave(
+                f"imgs/numbers/image_{i}.png",
+                jnp.concatenate(x_0, axis=1).squeeze(axis=2).astype(jnp.uint8),
+                cmap="binary",
+            )
 
 
 if __name__ == "__main__":
